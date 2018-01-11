5633469586bf84f22ed324f4f612e0cc15cdc952
mvpratt <mvpratt@gmail.com>
2018-01-11 12:22:15 -0700

    add error handling to kraken

--- server/api/apiKraken.js
+++ server/api/apiKraken.js
@@ -17,6 +17,15 @@ module.exports = {
         const obj = JSON.parse(value);
         const datetime = new Date(Date.now());
 
+        //if (obj.error ){
+        //    console.log('Error: Kraken parseResponse(): property .success is false');
+        //    console.log(obj);
+        //}
+        /*else */if( ('result' in obj) !== true || obj.result === undefined || obj.result === null ) {
+            console.log('Error: Kraken parseResponse(): property .result is undefined');
+            console.log(obj);
+        }
+        else {
         switch (ticker) {
         case module.exports.tickers.ETH:
             db.updatePrice('ETH', 'Kraken', datetime, obj.result.XETHXXBT.c[0]);
@@ -28,10 +37,11 @@ module.exports = {
             db.updatePrice('DASH', 'Kraken', datetime, obj.result.DASHXBT.c[0]);
             break;
         case module.exports.tickers.BTC:
-            db.updatePrice('BTC', 'Kraken', datetime, obj.result.XXBTZUSD.c[0]);
+            db.updatePrice('BTC', 'Kraken', datetime, obj.result.XXBTZUSD.c[0]);  /// todo - double check coin ticker
             break;        
         default:
-            console.log('Error');
+                console.log('Error: Kraken parseResponse(): Unsupported coin ticker');
+        }
         }
     },
 };
